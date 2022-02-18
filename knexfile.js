201bd08766930c844bf2b534ba48829e225fd96e
YuyaKiri <kiri3625@yahoo.co.jp>
2022-02-18 21:51:46 +0900

    My first commit

---
+++ knexfile.js
@@ -0,0 +1,47 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+ module.exports = {
+
+  development: {
+    client: "mysql",
+    connection: {
+      database: "todo_app",
+      user: "root",
+      password: "Ayumu0718",
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+  },
+
+  staging: {
+    client: "mysql",
+    connection: {
+      database: "todo_app",
+      user: "root",
+      password: "Ayumu0718",
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+  },
+
+  production: {
+    client: "mysql",
+    connection: {
+      database: "todo_app",
+      user: "root",
+      password: "Ayumu0718",
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+  }
+
+};
