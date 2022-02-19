d2dca13154e8eea0d8d48db76f434a0458379868
Kirinman <96939173+YuyaKiri@users.noreply.github.com>
2022-02-19 17:14:12 +0900

    Add files via upload

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,7 @@
 // Update with your config settings.
 
+//mysql://b12ad4b364fb46:672c111e@us-cdbr-east-05.cleardb.net/heroku_d6b8266eaa8bbb9?reconnect=true
+
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
@@ -8,9 +10,10 @@
   development: {
     client: "mysql",
     connection: {
-      database: "todo_app",
-      user: "root",
-      password: "Ayumu0718",
+      host: "us-cdbr-east-05.cleardb.net", 
+      database: "heroku_d6b8266eaa8bbb9",
+      user: "b12ad4b364fb46",
+      password: "672c111e",
     },
     pool: {
       min: 2,
